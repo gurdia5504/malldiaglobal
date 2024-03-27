e5702212027cdd0e6772e43331be6ec853d319f8
Soban Mughal <sobanmughal3312@gmail.com>
2024-03-27 14:15:25 +0500

    registration fixed

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,13 +34,13 @@ class RegisteredUserController extends Controller
         // return $currencies =SellerQuotes::all();
         $currencies = Dovizler::all();
         // $currencies = Currency::all();
-        $countries= Country::all();
-        $company_types= CompanyType::all();
-        $states= State::all();
+        $countries = Country::all();
+        $company_types = CompanyType::all();
+        $states = State::all();
         $cities = City::all();
         $regions = Region::all();
         //  dd($categories);
-        return view('auth.register', compact('categories', 'regions','currencies','countries','states','cities','company_types'));
+        return view('auth.register', compact('categories', 'regions', 'currencies', 'countries', 'states', 'cities', 'company_types'));
     }
 
     /**
@@ -60,13 +60,13 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-          // 'code' =>$this->ticket_number(),
+            // 'code' =>$this->ticket_number(),
             'last_name' => ['required', 'string', 'max:255'],
             'first_name' => ['required', 'string', 'max:255'],
             'birth_day' => 'required',
             'gender' => 'required',
-           // 'regions' => 'string',
-             'region'=>'string',
+            // 'regions' => 'string',
+            'region' => 'string',
             'country' => 'string',
             'state' => 'string',
             'city' => 'string',
@@ -78,11 +78,11 @@ class RegisteredUserController extends Controller
             'iban'         => 'required',
             'category'         => 'required',
             //  'currency'         => 'required',
-          //  'currency_id' => 'exists:currencies,id',
-           'seller_quotes_id'=> 'exists:seller_quotes,id',
-           'region_id'=> 'exists:regions,id',
-       //  'seller_quote' => ['required', 'numeric'],
-    //    'seller_quotes'=> 'required',
+            //  'currency_id' => 'exists:currencies,id',
+            //    'seller_quotes_id'=> 'exists:seller_quotes,id',
+            'region_id' => 'exists:regions,id',
+            //  'seller_quote' => ['required', 'numeric'],
+            //    'seller_quotes'=> 'required',
             //  'fx_rate'         => 'required',
 
             //  'trade_name'  => 'string',
@@ -98,65 +98,84 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-     // $seller_quotes = SellerQuotes::findOrFail($request['seller_quote']);
+        // $seller_quotes = SellerQuotes::findOrFail($request['seller_quote']);
 
-        $personal_info = new User();
-        $personal_info->code = $this->ticket_number();
-        $personal_info->first_name        = $request->first_name;
-        $personal_info->last_name         = $request->last_name;
-        $personal_info->birth_day =  $request->birth_day;
+        // $personal_info = new User();
+        $personal_info['code'] = $this->ticket_number();
+        $personal_info['first_name'] = $request->first_name;
+        $personal_info['last_name'] = $request->last_name;
+        $personal_info['birth_day'] = $request->birth_day;
+        $personal_info['gender'] = $request->gender;
+        $personal_info['region'] = $request->region;
+        $personal_info['country'] = $request->country;
+        $personal_info['state'] = $request->state;
+        $personal_info['city'] = $request->city;
+        $personal_info['phone'] = $request->phone;
+        $personal_info['address'] = $request->address;
+        $personal_info['tc_vkn'] = $request->tc_vkn;
+        $personal_info['bank_name'] = $request->country;
+        $personal_info['category'] = $request->category;
+        $personal_info['currency_id'] = $request->currency_id;
+        $personal_info['regoion_id'] = $request->region_id;
+        $personal_info['seller_quotes'] = $request->seller_quotes;
+        $personal_info['mersis_no'] = $request->mersis_no;
+        $personal_info['registration_number'] = $request->registration_number;
+        $personal_info['taxe_admin'] = $request->taxe_admin;
+        $personal_info['taxe_number'] = $request->mersis_no;
+        $personal_info['email'] = $request->email;
+        $personal_info['password'] = Hash::make($request->password);
 
-        $personal_info->gender = $request->gender;
-       // $personal_info->regions  = $request->regions;
-        $personal_info->region  = $request->region;
-        $personal_info->country  = $request->country;
+        // $personal_info->code = $this->ticket_number();
+        // $personal_info->first_name        = $request->first_name;
+        // $personal_info->last_name         = $request->last_name;
+        // $personal_info->birth_day =  $request->birth_day;
 
-        $personal_info->state = $request->state;
-        $personal_info->city = $request->city;
-        $personal_info->phone = $request->phone;
-        $personal_info->address = $request->address;
-        $personal_info->tc_vkn = $request->tc_vkn;
-        $personal_info->bank_name  = $request->bank_name;
-        $personal_info->iban    = $request->iban;
-        $personal_info->category    = $request->category;
-        $personal_info->currency_id    = $request->currency_id;
-      $personal_info->seller_quotes_id =$request->seller_quotes_id;
-      $personal_info ->regoion_id=$request->region_id;
-     // $personal_info->seller_quotes =$request->seller_quotes;
+        // $personal_info->gender = $request->gender;
+        // // $personal_info->regions  = $request->regions;
+        // $personal_info->region  = $request->region;
+        // $personal_info->country  = $request->country;
+
+        // $personal_info->state = $request->state;
+        // $personal_info->city = $request->city;
+        // $personal_info->phone = $request->phone;
+        // $personal_info->address = $request->address;
+        // $personal_info->tc_vkn = $request->tc_vkn;
+        // $personal_info->bank_name  = $request->bank_name;
+        // $personal_info->iban    = $request->iban;
+        // $personal_info->category    = $request->category;
+        // $personal_info->currency_id    = $request->currency_id;
+        // $personal_info->seller_quotes_id = $request->seller_quotes_id;
+        // $personal_info->regoion_id = $request->region_id;
+        // $personal_info->seller_quotes = $request->seller_quotes;
 
         //  'fx_rate'  => $request->fx_rate,
 
         // 'trade_name'  => $request->trade_name,
         // 'company_type' => $request->company_type,
-        $personal_info->mersis_no = $request->mersis_no;
-        $personal_info->registration_number = $request->registration_number;
-        $personal_info->taxe_admin  = $request->taxe_admin;
-        $personal_info->taxe_number = $request->taxe_number;
+        // $personal_info->mersis_no = $request->mersis_no;
+        // $personal_info->registration_number = $request->registration_number;
+        // $personal_info->taxe_admin  = $request->taxe_admin;
+        // $personal_info->taxe_number = $request->taxe_number;
 
-        //  'image_path'=>$request->image_path,
-        $personal_info->email = $request->email;
-        $personal_info->password = Hash::make($request->password);
+        // //  'image_path'=>$request->image_path,
+        // $personal_info->email = $request->email;
+        // $personal_info->password = Hash::make($request->password);
 
         if ($request->file('image_path')) {
-            $picture       = !empty($request->file('image_path')) ? $request->file('image_path')->getClientOriginalName() : '';
+            $picture = !empty($request->file('image_path')) ? $request->file('image_path')->getClientOriginalName() : '';
             $request->file('image_path')->move(public_path('assets/images/'), $picture);
+            $personal_info['image_path'] = isset($picture) && !empty($picture) ? $picture : '';
         }
-        $personal_info->image_path        = isset($picture) && !empty($picture) ? $picture : '';
-   $personal_info->save();
 
+        // $personal_info->save();
+        // dd($personal_info);
+        $user = User::create($personal_info);
 
-// $seller_quotes->users()->save($personal_info);
-
-
-
-
-
+        // $seller_quotes->users()->save($personal_info);
 
         event(new Registered($personal_info));
-
-
-        Auth::login($personal_info);
-
+        Auth::login($user);
+        // return redirect()->intended(RouteServiceProvider::HOME);
         return redirect(RouteServiceProvider::HOME);
     }
 }
